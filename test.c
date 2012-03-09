ba96cd5f86345acc58eff9c2431266638951c089
Jinglei Ren <jinglei.ren@gmail.com>
2012-03-09 09:40:42 +0800

    Add .gitignore.

---
+++ test.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012 Jinglei Ren <jinglei.ren@stanzax.org>
+ * All rights reserved.
+ */
+
+//
+//  test.c
+//  cinquain-meta
+//
+//  Created by Jinglei Ren <jinglei.ren@gmail.com> on 3/9/12.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
+
